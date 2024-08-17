3b2a7730352800647eb4dd2366d5735980a821cd
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2024-08-17 03:40:10 +0000

    bf

--- index.js
+++ index.js
@@ -32,12 +32,15 @@ const client = new Client({
   ]
 });
 
+const ignoreCommandFiles = ['filter.js'];
+
 client.commands = new Collection();
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
+  if (ignoreCommandFiles.includes(file)) continue;
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
   client.commands.set(command.data.name, command);
