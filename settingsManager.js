5f0b990a6ffffa2b999a36356acab89ec9dbf319
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2024-08-23 22:38:24 +0900

    bf

--- settingsManager.js
+++ settingsManager.js
@@ -3,13 +3,13 @@ const db = require('./database');
 // 設定の保存
 async function saveSettings(guildId, settings) {
     await db.query(
-        `INSERT INTO settings (guild_id, volume, loop_state, filters) 
+        `INSERT INTO settings (guild_id, volume, loop_state, equalizer) 
          VALUES (?, ?, ?, ?) 
          ON DUPLICATE KEY UPDATE 
          volume = VALUES(volume), 
          loop_state = VALUES(loop_state), 
-         filters = VALUES(filters)`,
-        [guildId, settings.volume, settings.loopState, settings.filters.join(',')]
+         equalizer = VALUES(equalizer)`,
+        [guildId, settings.volume, settings.loopState, settings.equalizer]
     );
 }
 
