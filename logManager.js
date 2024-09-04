0a2604df0f946a1bd7ac917cd5ef37c09d238098
lovetwice1012 <lovetwice1012@gmail.com>
2024-09-05 00:30:53 +0900

    test

--- logManager.js
+++ logManager.js
@@ -39,7 +39,7 @@ async function logAction(guildId, userId, commandName, actionType, details) {
     await ensureActionTypeExists(actionType);
 
     const query = `
-    INSERT INTO bot_logs (guild_id, user_id, command_name, action_type, action_details, timestamp)
+    INSERT INTO bot_logs (guild_id, user_id, command_name, action_type, timestamp)
     VALUES (?, ?, ?, ?, ?, NOW())
   `;
     const params = [guildId, userId, commandName, actionType, JSON.stringify(details, CircularCheckAndReplaceBigIntToString)];
